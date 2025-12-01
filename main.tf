95e5a35f0a1fa2dda20e4f2432cfb5f0d237d90e
DorZion51 <37267616+DorZion51@users.noreply.github.com>
2025-12-01 16:02:39 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "aws_s3_bucket" "financials" {
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-financials"
+  region        = "us-west-2"
+  acl           = "private"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-financials"
+    Environment = local.resource_prefix.value
+  }
+
+}
